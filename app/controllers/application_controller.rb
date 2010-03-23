455786df7f3fde0ee0e54c2b7103455cec9a2288
Forrest Ye <forresty1986@gmail.com>
2010-03-23 22:01:00 +0800

    authentication filter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,9 +2,30 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+  before_filter :fetch_logged_in_user
   helper :all # include all helpers, all the time
-  protect_from_forgery # See ActionController::RequestForgeryProtection for details
+
+  # See ActionController::RequestForgeryProtection for details
+  protect_from_forgery 
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
+  filter_parameter_logging :password
+
+protected
+  def fetch_logged_in_user
+    return unless session[:user_id]
+    @current_user = User.find_by_id(session[:user_id])
+  end
+  
+  def logged_in?
+    ! @current_user.nil?
+  end
+
+  def login_required
+    return true if logged_in?
+    session[:return_to] = request.request_uri
+    redirect_to new_session_path and return false
+  end
+
+  helper_method :logged_in?
 end
