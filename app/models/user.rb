72548281ca14bef18f5581677b4ac9fd642c61ba
Forrest Ye <forresty1986@gmail.com>
2010-03-23 22:45:55 +0800

    user.recorded_today?

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,13 @@
 class User < ActiveRecord::Base
+  has_many :wakeup_records
+
   def self.authenticate(login, password)
     user = self.find_by_username_and_password(login, password) 
     user = self.find_by_email_and_password(login, password) unless user
     return user
+  end
+
+  def recorded_today?
+    WakeupRecord.recorded_today?(self)
   end
 end
--- app/models/wakeup_record.rb
+++ app/models/wakeup_record.rb
@@ -1,2 +1,13 @@
 class WakeupRecord < ActiveRecord::Base
+  belongs_to :user
+
+  def self.recorded_today?(user)
+    last = user.wakeup_records.last
+    !last.nil? and last.recorded_today?
+  end
+
+private
+  def recorded_today?
+    # TODO to be implemented
+  end
 end
