aef2f53510eb2986f95b751bd01139398f3e13f3
Forrest Ye <forresty1986@gmail.com>
2010-03-23 21:45:48 +0800

    session control and layout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @current_user = User.authenticate(params[:login], params[:password])
+    if @current_user
+      session[:user_id] = @current_user.id
+      redirect_to :root
+    else
+      flash.now[:notice] = "用户名或者密码不正确"
+      render :action => :new
+    end
   end
 
   def destroy
+    session[:user_id] = @current_user = nil
+    flash[:notice] = '已退出登录'
+    redirect_to :root
   end
-
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  def self.authenticate(login, password)
+    user = self.find_by_username_and_password(login, password) 
+    user = self.find_by_email_and_password(login, password) unless user
+    return user
+  end
 end
