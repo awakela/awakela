690d63d6a92186b3ffad7758a99e7990a4040111
Forrest Ye <forresty1986@gmail.com>
2010-03-23 20:48:04 +0800

    home controller

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ ActionController::Routing::Routes.draw do |map|
 
   map.resource  :session
 
+  map.root      :home
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
